2516884f7cd925df20793913a25a051a14b1eb38
hassanmujtaba <hassan03203406130@gmail.com>
2020-04-24 04:35:17 +0500

    introduction and progress added

--- website clone 3rd assignment/app.js
+++ website clone 3rd assignment/app.js
@@ -1,13 +1,10 @@
+function scrollAppear(){
+  var text = document.querySelector(".text");
+  var textPosition = text.getBoundingClientRect().top;
+  var screenPosition = window.innerHeight / 1;
 
-window.onscroll = function() {myFunction()};
-
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
+  if (textPosition < screenPosition){
+      text.classList.add('text-appear')
   }
 }
+window.addEventListener("scroll",scrollAppear)
