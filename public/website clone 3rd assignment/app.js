cadda61bcbb9ed419372082659792aa4ce900984
hassanmujtaba <hassan03203406130@gmail.com>
2020-07-11 20:40:14 +0500

    add all the files in the public folder

---
+++ public/website clone 3rd assignment/app.js
@@ -0,0 +1,11 @@
+alert("This Website is Full Responsive")
+function scrollAppear(){
+  var text = document.querySelector(".text");
+  var textPosition = text.getBoundingClientRect().top;
+  var screenPosition = window.innerHeight / 1;
+
+  if (textPosition < screenPosition){
+      text.classList.add('text-appear')
+  }
+}
+window.addEventListener("scroll",scrollAppear)
